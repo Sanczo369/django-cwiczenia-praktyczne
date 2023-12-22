1488a34a6c4a1228afcab1f679ad33a2aa41aea7
Arek Sanecki <areksanecki@gmail.com>
2023-12-22 21:47:10 +0100

    dodanie modelu Category i News. Wykonanie migracji. Dodanie 3 kategorii oraz 3 newsów

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from news.models import Category, News
+# dla kategorii
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','icon')
+    prepopulated_fields = {'slug': ('name',)}
+    
+# dla wiadomości
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title','date')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields =  ['title', 'text']
+
+    
+# rejestracja wraz z podaniem klasy konfigurującej PA
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)
