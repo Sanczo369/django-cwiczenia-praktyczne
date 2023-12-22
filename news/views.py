b140d64b1a6e3a95f331a934e5e51e4cd6ade23a
Arek Sanecki <areksanecki@gmail.com>
2023-12-22 22:42:02 +0100

    dodanie index.html zawierajacą liste newsów

--- news/views.py
+++ news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import News
 # Create your views here.
+def news(request):
+    news = News.objects.all().order_by('-id')
+    context = {
+        'news': news,
+        }
+    return render(request,'index.html',context)
